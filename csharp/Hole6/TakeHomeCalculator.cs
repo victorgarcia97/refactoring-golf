85f336d265b916c815eee83460b55db934de4fb0
Victor Garcia <vgarcia@aidacanarias.com>
2023-03-10 14:22:54 +0100

    Refact: Hole 6 to Hole 7

--- csharp/Hole6/TakeHomeCalculator.cs
+++ csharp/Hole6/TakeHomeCalculator.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 
 namespace Hole6
 {
@@ -14,15 +13,7 @@ namespace Hole6
 
         public Money NetAmount(Money first, params Money[] rest)
         {
-            List<Money> monies = rest.ToList();
-
-            Money total = first;
-
-            foreach (Money next in monies)
-            {
-                total = total.Plus(next);
-            }
-
+            Money total = rest.Aggregate(first, (money, x) => money.Plus(x));
             Money tax = taxRate.Apply(total);
             return total.Minus(tax);
         }
