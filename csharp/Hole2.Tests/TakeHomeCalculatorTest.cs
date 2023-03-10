b41605aecf45b6f02544a70d82d1c683b68c9c3d
Victor Garcia <vgarcia@aidacanarias.com>
2023-03-10 12:38:03 +0100

    Refact: hole2 to hole 3

--- csharp/Hole2.Tests/TakeHomeCalculatorTest.cs
+++ csharp/Hole2.Tests/TakeHomeCalculatorTest.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 
 namespace Hole2.Tests
@@ -8,9 +7,9 @@ namespace Hole2.Tests
         [Fact]
         public void CanCalculateTax()
         {
-            int first = new TakeHomeCalculator(10).NetAmount(new Pair<int, String>(40, "GBP"),
-                new Pair<int, String>(50, "GBP"),
-                new Pair<int, String>(60, "GBP")).first;
+            int first = new TakeHomeCalculator(10).NetAmount(new Money(40, "GBP"),
+                new Money(50, "GBP"),
+                new Money(60, "GBP")).value;
             Assert.Equal(135, first);
         }
 
@@ -18,7 +17,7 @@ namespace Hole2.Tests
         public void CannotSumDifferentCurrencies()
         {
             Assert.Throws<Incalculable>(() => new TakeHomeCalculator(10).NetAmount(new
-                Pair<int, String>(4, "GBP"), new Pair<int, String>(5, "USD")));
+                Money(4, "GBP"), new Money(5, "USD")));
         }
     }
 }
--- csharp/Hole2/TakeHomeCalculator.cs
+++ csharp/Hole2/TakeHomeCalculator.cs
@@ -13,34 +13,34 @@ namespace Hole2
             this.percent = percent;
         }
 
-        public Pair<int, String> NetAmount(Pair<int, String> first, params Pair<int, String>[] rest)
+        public Money NetAmount(Money first, params Money[] rest)
         {
-            List<Pair<int, String>> pairs = rest.ToList();
+            List<Money> monies = rest.ToList();
 
-            Pair<int, String> total = first;
+            Money total = first;
 
-            foreach (Pair<int, String> next in pairs)
+            foreach (Money next in monies)
             {
-                if (!next.second.Equals(total.second))
+                if (!next.currency.Equals(total.currency))
                 {
                     throw new Incalculable();
                 }
             }
 
-            foreach (Pair<int, String> next in pairs)
+            foreach (Money next in monies)
             {
-                total = new Pair<int, String>(total.first + next.first, next.second);
+                total = new Money(total.value + next.value, next.currency);
             }
 
-            Double amount = total.first * (percent / 100d);
-            Pair<int, String> tax = new Pair<int, String>(Convert.ToInt32(amount), first.second);
+            Double amount = total.value * (percent / 100d);
+            Money tax = new Money(Convert.ToInt32(amount), first.currency);
 
-            if (!total.second.Equals(tax.second))
+            if (!total.currency.Equals(tax.currency))
             {
                 throw new Incalculable();
             }
 
-            return new Pair<int, String>(total.first - tax.first, first.second);
+            return new Money(total.value - tax.value, first.currency);
         }
     }
 }
